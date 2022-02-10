a32073e261f37108eb1a7c7185e0f2da40f2eb3b
Bia <97973267+B-3ia@users.noreply.github.com>
2022-02-10 10:36:35 +0100

    Verifica bisestile

--- 09.24es1.c
+++ 09.24es1.c
@@ -1,7 +1,7 @@
 /*Scrivere un programma che chiede all'utente di inserire un numero 
 intero che corrisponde ad un anno e visualizza 1 se l'anno è 
 bisestile, 0 altrimenti. NOTA: un anno è bisestile se è divisibile 
-per 4 ma non per 100 o se è divisibile per 400.*/
+per 4 ma non per 100 o se è divisibile per 400*/
 
 #include<stdio.h>
 #define COST 4
