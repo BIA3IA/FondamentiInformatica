f099b8eb4281a353fabcc2efbbd859bbd76896f7
White <97973267+SnoowWhite@users.noreply.github.com>
2022-05-23 11:45:55 +0200

    Verifica stringhe uguali

--- 10.08es4.c
+++ 10.08es4.c
@@ -21,3 +21,32 @@ int main() {
 	
 	return 0;
 } 
+
+OPPURE 
+
+#include <stdio.h>
+#define LUNS 30
+
+int main(){
+	char str[LUNS+1], str2[LUNS+1];
+	int i, j, uguali;
+
+	scanf("%s %s", str, str2);
+
+	for(j=0; str[j]!='\0'; j++);
+
+	for(i=0; str2[i]!='\0'; i++);
+	
+	if(i!=j){
+		uguali=0;
+	} else {
+		for(i=0, uguali=1; str[i]!='\0' && uguali; i++)
+			if(str[i]!=str2[i])
+				uguali=0;
+	}
+
+	printf("%d\n", uguali);
+
+
+	return 0;
+}
