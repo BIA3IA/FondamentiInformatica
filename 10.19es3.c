39bb5e9e98b28561f6abae6aa58892dfad70a3b3
White <97973267+SnoowWhite@users.noreply.github.com>
2022-05-25 17:20:47 +0200

    Sottoprogramma da int a media float

--- 10.19es3.c
+++ 10.19es3.c
@@ -3,29 +3,54 @@ interi e restituisce la loro media (un numero float).
 Scrivere un programma che chiede all'utente due coppie di interi e
 calcola e visualizza la media di ciascuna di esse.*/
 
-#include<stdio.h>
+#include <stdio.h>
+#define NUM 2.0
 
 float media (int, int);
 
-int main() {
+int main(){
 	int a, b;
-	float m;
+	float med;
 
 	scanf("%d %d", &a, &b);
-	m=media(a, b);
-	printf("%f", m);
+	printf("%f", media(a,b));
 	
 	scanf("%d %d", &a, &b);
-	m=media(a, b);
-	printf("%f", m);\
+	printf("%f", media(a,b));
 	
 	return 0;
 }
 
-float media(int n, int k){
-	float m;
+float media (int n, int k){
 
-	m=(n+k)/2;
+	return (n+k)/NUM;
+}
 
-	return m;
-} 
+/* OPPURE
+
+#include <stdio.h>
+
+float media (int, int);
+
+int main(){
+	int a, b;
+	float med;
+
+	scanf("%d %d", &a, &b);
+
+	printf("%f", media(a,b));
+
+	
+	return 0;
+}
+
+float media (int n, int k){
+	float a, b;
+	
+	a=(float)n;
+	b=(float)k;
+	
+	return (a+b)/2;
+}
+
+*/
