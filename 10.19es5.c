375ece9040da851ebd4f3c7770860c0ecb417f03
White <97973267+SnoowWhite@users.noreply.github.com>
2022-05-26 11:24:19 +0200

    Sottoprogramma Goldbach

--- 10.19es5.c
+++ 10.19es5.c
@@ -8,41 +8,36 @@ Scrivere un programma che chiede all'utente un numero n maggiore di
 2 e pari (nel caso richiede il numero) e calcola e visualizza tutte
 le possibili coppie di numeri primi la cui somma è uguale ad n.*/
 
-#include<stdio.h>
+#include <stdio.h>
 
 int primo(int);
 int input();
 
-int main() {
-	int n;
+int main(){
+	int n, i;
+	float k;
 
-	n=input();
-
-	for(i=2; i<=n/2; i++){			//i<=n/2 perche se no i numeri si ripetono uguali
-		if(primo(i) && primo(n-i))
-			printf("%d %d", i, i-n);
+	scanf("%d", &n);
+	
+	while(n<=2 || n%2){
+		scanf("%d", &n);
 	}
 	
+	for(i=1; i<=n/2; i++){ 			// i<=n/2 se no ripeto i numeri
+		if(primo(i) && primo(n-i)){
+			printf("%d %d\n", n-i, i);
+		}
+	}
+
 	return 0;
 }
 
 int primo(int n){
-	int p, i;
+	int i, primo;
 
-	for(i=2, p=1; i*i<=n  && p; i++){
-		if(!(n%i))
-			p=0;
-	}
+	for(i=2, primo=1; i*i<=n && primo; i++)
+		if(n%i==0)
+			primo=0;
 
-	return p;
+	return primo;
 }
-
-int input(){
-	int n;
-
-	do {
-		scanf("%d", &n);
-	} while (n<=2 && n%2);
-
-	return n;
-} 
